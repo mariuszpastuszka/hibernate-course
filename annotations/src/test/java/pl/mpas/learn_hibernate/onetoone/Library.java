1cb19954530372ddad7ee5c2cd5352e365ff6ccf
maniek <mpastuszka@gmail.com>
2018-07-06 17:46:59 +0200

    [add] - adding onetoone

---
+++ annotations/src/test/java/pl/mpas/learn_hibernate/onetoone/Library.java
@@ -0,0 +1,61 @@
+package pl.mpas.learn_hibernate.onetoone;
+
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Library {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    public Library() {
+    }
+
+    public Library(String name) {
+        super();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+}
